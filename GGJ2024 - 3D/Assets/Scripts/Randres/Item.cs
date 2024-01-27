3bfa001d72fb0bb9112763fb603a8807a759a47e
Randres <randresho@gmail.com>
2024-01-27 13:33:09 -0600

    Sistema de interaccion en proceso

--- GGJ2024 - 3D/Assets/Scripts/Randres/Item.cs
+++ GGJ2024 - 3D/Assets/Scripts/Randres/Item.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ItemType { Dialogo, Agarrable,}
 public class Item : MonoBehaviour
 {
+    public ItemType itemType; 
     // Start is called before the first frame update
     void Start()
     {
--- GGJ2024 - 3D/Assets/Scripts/Randres/PlayerActions.cs
+++ GGJ2024 - 3D/Assets/Scripts/Randres/PlayerActions.cs
@@ -7,7 +7,8 @@ public class PlayerActions : MonoBehaviour
 {
     private Rigidbody m_rigidbody;
     private PlayerController m_playerController;
-
+    public Transform attachPoint;
+    public bool isAttach;
 
     // Start is called before the first frame update
     void Awake()
@@ -27,7 +28,14 @@ public class PlayerActions : MonoBehaviour
         GameObject obj = other.gameObject;
         if (other.GetComponent<Item>() != null)
         {
-            
+            if(!isAttach)
+            {
+                print("Se puede agarrar");
+            }
+            else
+            {
+                print("No se puede agarrar");
+            }
         }
     }
 
@@ -35,10 +43,18 @@ public class PlayerActions : MonoBehaviour
     private void OnEnable()
     {
         m_playerController.Enable();
+        m_playerController.Move.Interaccion.performed += UseAction;
     }
+
+    private void UseAction(InputAction.CallbackContext callbackContext)
+    {
+
+    }
+
     private void OnDisable()
     {
         m_playerController.Disable();
+        m_playerController.Move.Interaccion.performed -= UseAction;
     }
     #endregion
 }
