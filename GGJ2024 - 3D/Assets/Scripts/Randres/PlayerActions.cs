e5faddfd59a1685a48d30b313439d41f9030d2b0
Randres <randresho@gmail.com>
2024-01-27 11:54:29 -0600

    Correccion de tamaños de objetos

---
+++ GGJ2024 - 3D/Assets/Scripts/Randres/PlayerActions.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerActions : MonoBehaviour
+{
+    private Rigidbody m_rigidbody;
+    private PlayerController m_playerController;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_playerController = new PlayerController();
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Input Enable / Disable
+    private void OnEnable()
+    {
+        m_playerController.Enable();
+    }
+    private void OnDisable()
+    {
+        m_playerController.Disable();
+    }
+    #endregion
+}
